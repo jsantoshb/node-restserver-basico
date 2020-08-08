f52ec5116eae713806b5971868921e1c3fcfef6d
jsantoshb <jsantos@hb.com.mx>
2020-08-07 22:11:14 -0700

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,5 +11,5 @@ process.env.NODE_ENV = process.env.NODE_ENV  || 'dev'
 // ======================
 let urlDB = process.env.NODE_ENV === 'dev' 
     ?  'mongodb://localhost:27017/cafe'
-    : 'mongodb+srv://sa:qAP5usAwLirY8MjG@cluster0.juopv.mongodb.net/cafe?retryWrites=true&w=majority'
+    : process.env.MONGO_DBURL
 process.env.URLDB = urlDB;
